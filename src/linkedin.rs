7b28cc06d6ec1e1f58eeb4c81713061c146f60d4
Jaroslaw Nowosad <yarenty@gmail.com>
2022-02-18 11:42:51 +0000

    linked tutorial

---
+++ src/linkedin.rs
@@ -0,0 +1,21 @@
+pub fn min_max(){
+    let arr = [1,2,3,4,4];
+    let mut min: i32;
+    let mut max: i32;
+    let mut mean: f64;
+    
+    
+    mean = 0.0;
+    min = arr[0];
+    max = arr[0];
+    // for n in arr {
+    for &n in arr.iter() {
+        if n < min {min = n;}
+        if n > max {max = n;}
+        mean += n as f64;
+    }
+    
+    mean /=  arr.len() as f64;
+    
+    println!(" min = {};  max = {}; mean = {}", min, max, mean);
+}
